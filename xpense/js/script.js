55f4f27284d4b3bbe52fafb9dd0c46cde26fec2f
vdovuhin <112893071+vdovuhin@users.noreply.github.com>
2025-02-28 19:02:04 +0300

    Add files via upload

--- xpense/js/script.js
+++ xpense/js/script.js
@@ -1,10 +1,57 @@
+// Строгий режим
+"use strict"
 
-let documentActions = (e) => {
+window.addEventListener('load', windowLoad);
+const html = document.documentElement;
+
+function windowLoad() {
+	document.addEventListener('click', documentActions);	
+	slidersInit()
+}
+function documentActions(e) {
+	const type = e.type
 	const targetElement = e.target
 
-	if (targetElement.closest('.icon-menu')) {
-		document.documentElement.classList.toggle('open-menu')
+	if (type === 'click') {
+		// Меню-бургер
+		if (targetElement.closest('.icon-menu')) {
+			document.documentElement.classList.toggle('open-menu')
+		}
+		targetElement.closest('.menu__link') && html.classList.contains('menu-open') ? html.classList.remove('menu-open') : null
+
 	}
 }
+//Slides
 
-document.addEventListener('click', documentActions)
+function slidersInit() {
+	const swiper = new Swiper('.slide__swiper-slide', {
+
+
+		// Optional parameters
+		direction: 'vertical',
+		loop: true,
+
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		// And if we need scrollbar
+		scrollbar: {
+			el: '.swiper-scrollbar',
+		},
+	});
+
+
+
+}
+
+
+
+
